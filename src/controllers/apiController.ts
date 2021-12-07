cce470f48074ca4e133dd179100fd2ecd99f716f
Cezar Molinar <cezarmolinar@gmail.com>
2021-12-07 17:15:56 -0300

    Criação rotas CRUD

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,7 +1,62 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
 import { Phrase } from '../models/Phrase';
 
 export const ping = (req: Request, res: Response) => {
     res.json({ pong: true });
 }
+
+export const createPhrase = async (req: Request, res: Response) => {
+    const { author, txt } = req.body;
+
+    const newPhrase = Phrase.build({
+        author: author,
+        txt: txt
+    });
+    await newPhrase.save();
+
+    res.status(201).json(newPhrase);
+}
+
+export const listPhrases = async (req: Request, res: Response) => {
+    let list = await Phrase.findAll({});
+    res.status(200).json(list);
+}
+
+export const getPhrase = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    let list = await Phrase.findByPk(id);
+    if (list) {
+        res.status(200).json(list);
+    } else {
+        res.status(200).json({ error: 'Frase não encontrada' });
+    }
+}
+
+export const updatePhrase = async (req: Request, res: Response) => {
+    const id = req.params.id;
+
+    let phrase = await Phrase.findByPk(id);
+    if (phrase) {
+        phrase.author = req.body.author;
+        phrase.txt = req.body.txt;
+        await phrase.save();
+
+        res.status(200).json(phrase);
+    } else {
+        res.status(200).json({ error: 'Frase não encontrada' });
+    }
+}
+
+export const deletePhrase = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    let phrase = await Phrase.findByPk(id);
+    if (phrase) {
+        await phrase.destroy();
+        res.status(204).json({});
+    } else {
+        res.status(200).json({ error: 'Frase não encontrada' });
+    }
+}
+
+
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,15 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
+
+import * as ApiController from '../controllers/apiController';
 
 const router = Router();
 
-router.get('/ping', (req: Request, res: Response) => {
-    res.json({ pong: true });
-});
+router.get('/ping', ApiController.ping);
+
+router.post('/frase', ApiController.createPhrase);
+router.get('/frase', ApiController.listPhrases);
+router.get('/frase/:id', ApiController.getPhrase);
+router.put('/frase/:id', ApiController.updatePhrase);
+router.delete('/frase/:id', ApiController.deletePhrase);
 
 export default router;
